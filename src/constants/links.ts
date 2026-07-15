1a1e1b1428d7a04bf82698e6e1b72dce7dc271ab
hager1nady <hajrnady24@gmail.com>
2026-07-15 19:48:16 +0300

    Improve links

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,7 +1,7 @@
-export const WHATSAPP_URL = "https://wa.me/1234567890";
-export const INSTAGRAM_URL = "https://instagram.com";
-export const TIKTOK_URL = "https://tiktok.com";
+export const WHATSAPP_URL = "https://wa.me/966501900981";
+export const INSTAGRAM_URL ="https://www.instagram.com/prive.shopper.jed/";
+export const TIKTOK_URL = "https://www.tiktok.com/@contact.priveshopper";
 
 // Used only by the Customer Information form to build a pre-filled WhatsApp
 // message link. Change this number when a real one is available.
-export const WHATSAPP_NUMBER = "966XXXXXXXXX";
+export const WHATSAPP_NUMBER = "966501900981";
